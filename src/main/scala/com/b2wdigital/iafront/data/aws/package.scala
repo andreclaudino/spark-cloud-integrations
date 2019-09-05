43caa2763e98ad00ae25d4cc9966bbd528b35206
André Claudino <andre.claudino@b2wdigital.com>
2019-09-05 20:11:30 -0300

    Renomeia pacote `athena` para `bigquery`

---
+++ src/main/scala/com/b2wdigital/iafront/data/aws/package.scala
@@ -0,0 +1,23 @@
+package com.b2wdigital.iafront.data
+
+import com.b2wdigital.iafront.data.configuration.ConfigurationUtils.{setSparkConf, setHadoopConf}
+import org.apache.spark.sql.SparkSession
+
+package object aws {
+
+  implicit class AthenaConfig(sparkSession: SparkSession) {
+
+    def setupAWS:Unit = {
+      sparkSession.conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+//      sparkSession.conf.set("spark.hadoop.fs.s3a.fast.upload", "true")
+//      sparkSession.conf.set("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
+
+      setSparkConf("spark.hadoop.fs.s3a.access.key", "AWS_ACCESS_KEY_ID")(sparkSession)
+      setSparkConf("spark.hadoop.fs.s3a.secret.key", "AWS_SECRET_ACCESS_KEY")(sparkSession)
+
+      setSparkConf("spark.hadoop.fs.s3a.awsAccessKeyId", "AWS_ACCESS_KEY_ID")(sparkSession)
+      setSparkConf("spark.hadoop.fs.s3a.sawsSecretAccessKey", "AWS_SECRET_ACCESS_KEY")(sparkSession)
+    }
+  }
+
+}
