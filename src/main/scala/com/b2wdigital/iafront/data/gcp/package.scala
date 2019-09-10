0f3efb913e2b4c6ab4094489375a6bb30133fb82
André Claudino <andre.claudino@b2wdigital.com>
2019-09-10 11:21:12 -0300

    Remove classes do bigquery, que não têm suporte nessa versão.

--- src/main/scala/com/b2wdigital/iafront/data/gcp/package.scala
+++ src/main/scala/com/b2wdigital/iafront/data/gcp/package.scala
@@ -5,52 +5,19 @@ import org.apache.spark.sql.{DataFrame, DataFrameReader, SparkSession}
 
 package object gcp {
 
-  implicit class BigQueryReaderExtensions(reader:DataFrameReader) {
-
-    def bigquery(table:String, billingProjectOption:Option[String]=None, dataProjectOption:Option[String]=None):DataFrame = {
-
-      val project =
-        dataProjectOption match {
-          case None =>
-            val Array(value, _) = table.split(":")
-            value
-
-          case Some(value) => value
-        }
-
-      val billingProject =
-        billingProjectOption match {
-          case None => project
-          case Some(value) => value
-        }
-
-      reader
-        .format("bigquery")
-        .option("table", table)
-        .option("project", project)
-        .option("parentProject", billingProject)
-        .load()
-    }
-  }
-
-  implicit class BigQueryWriterExtensions(dataFrame:DataFrame)(implicit sparkSession:SparkSession) {
-
-    def toBigQuery(table:String) = {
-      dataFrame.write.format("bigquery").saveAsTable(table)
-    }
-  }
-
-  implicit class BigQueryConfig(sparkSession: SparkSession) {
+  implicit class GCPConfig(sparkSession: SparkSession) {
     private val hadoopConfig = sparkSession.sparkContext.hadoopConfiguration
 
     def setupGCP:Unit = {
+      hadoopConfig.set("google.cloud.auth.service.account.enable", "true")
+      setHadoopConf("fs.gs.project.id", "GCP_PROJECT_ID")(sparkSession)
+
+      setHadoopConf("google.cloud.auth.service.account.json.keyfile", "GOOGLE_APPLICATION_CREDENTIALS_FILE")(sparkSession)
+
+      hadoopConfig.set("fs.gs.inputstream.support.gzip.encoding.enable", "true")
       hadoopConfig.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
       hadoopConfig.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
-      hadoopConfig.set("google.cloud.auth.service.account.enable", "true")
 
-      setHadoopConf("fs.gs.project.id", "GCP_PROJECT_ID")(sparkSession)
-      setHadoopConf("google.cloud.auth.service.account.json.keyfile",
-        "GOOGLE_APPLICATION_CREDENTIALS_FILE")(sparkSession)
     }
   }
 }
